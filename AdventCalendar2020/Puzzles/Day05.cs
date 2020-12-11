0e253d24f251ef9007d24ed3b701b6a579b94d8e
SirMartin <sirmartin@gmail.com>
2020-12-11 09:35:17 +0100

    Finished Day 5

--- AdventCalendar2020/Puzzles/Day05.cs
+++ AdventCalendar2020/Puzzles/Day05.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AdventCalendar2020.Puzzles
 {
@@ -20,82 +22,160 @@ namespace AdventCalendar2020.Puzzles
         }
 
         /// <summary>
-        /// --- Day 1: Report Repair ---
-        ///After saving Christmas five years in a row, you've decided to take a vacation at a nice resort on a tropical island. Surely, Christmas will go on without you.
-        ///
-        ///The tropical island has its own currency and is entirely cash-only.The gold coins used there have a little picture of a starfish; the locals just call them stars.None of the currency exchanges seem to have heard of them, but somehow, you'll need to find fifty of these coins by the time you arrive so you can pay the deposit on your room.
-        ///
-        ///
-        ///To save your vacation, you need to get all fifty stars by December 25th.
-        ///
-        ///Collect stars by solving puzzles.Two puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete the first.Each puzzle grants one star. Good luck!
-        ///
-        ///
-        ///Before you leave, the Elves in accounting just need you to fix your expense report (your puzzle input); apparently, something isn't quite adding up.
-        ///
-        ///Specifically, they need you to find the two entries that sum to 2020 and then multiply those two numbers together.
-        ///
-        ///For example, suppose your expense report contained the following:
-        ///
-        ///1721
-        ///979
-        ///366
-        ///299
-        ///675
-        ///1456
-        ///In this list, the two entries that sum to 2020 are 1721 and 299. Multiplying them together produces 1721 * 299 = 514579, so the correct answer is 514579.
-        ///
-        ///Of course, your expense report is much larger. Find the two entries that sum to 2020; what do you get if you multiply them together?
+        /// --- Day 5: Binary Boarding ---
+        /// You board your plane only to discover a new problem: you dropped your boarding pass! You aren't sure which seat is yours, and all of the flight attendants are busy with the flood of people that suddenly made it through passport control.
+        /// 
+        /// You write a quick program to use your phone's camera to scan all of the nearby boarding passes (your puzzle input); perhaps you can find your seat through process of elimination.
+        /// 
+        /// Instead of zones or groups, this airline uses binary space partitioning to seat people.A seat might be specified like FBFBBFFRLR, where F means "front", B means "back", L means "left", and R means "right".
+        /// 
+        /// The first 7 characters will either be F or B; these specify exactly one of the 128 rows on the plane(numbered 0 through 127). Each letter tells you which half of a region the given seat is in. Start with the whole list of rows; the first letter indicates whether the seat is in the front(0 through 63) or the back(64 through 127). The next letter indicates which half of that region the seat is in, and so on until you're left with exactly one row.
+        /// 
+        /// For example, consider just the first seven characters of FBFBBFFRLR:
+        /// 
+        /// Start by considering the whole range, rows 0 through 127.
+        /// F means to take the lower half, keeping rows 0 through 63.
+        /// B means to take the upper half, keeping rows 32 through 63.
+        /// F means to take the lower half, keeping rows 32 through 47.
+        /// B means to take the upper half, keeping rows 40 through 47.
+        /// B keeps rows 44 through 47.
+        /// F keeps rows 44 through 45.
+        /// The final F keeps the lower of the two, row 44.
+        /// The last three characters will be either L or R; these specify exactly one of the 8 columns of seats on the plane(numbered 0 through 7). The same process as above proceeds again, this time with only three steps.L means to keep the lower half, while R means to keep the upper half.
+        /// 
+        /// For example, consider just the last 3 characters of FBFBBFFRLR:
+        /// 
+        /// Start by considering the whole range, columns 0 through 7.
+        /// R means to take the upper half, keeping columns 4 through 7.
+        /// L means to take the lower half, keeping columns 4 through 5.
+        /// The final R keeps the upper of the two, column 5.
+        /// So, decoding FBFBBFFRLR reveals that it is the seat at row 44, column 5.
+        /// 
+        /// Every seat also has a unique seat ID: multiply the row by 8, then add the column.In this example, the seat has ID 44 * 8 + 5 = 357.
+        /// 
+        /// Here are some other boarding passes:
+        /// 
+        /// BFFFBBFRRR: row 70, column 7, seat ID 567.
+        /// FFFBBBFRRR: row 14, column 7, seat ID 119.
+        /// BBFFBBFRLL: row 102, column 4, seat ID 820.
+        /// As a sanity check, look through your list of boarding passes.What is the highest seat ID on a boarding pass?
         /// </summary>
         private int RunPuzzle1()
         {
             var inputLines = GetInputLines();
 
-            for (var i = 0; i < inputLines.Length; i++)
-            {
-                var a = Convert.ToInt32(inputLines[i]);
-                for (var k = i; k < inputLines.Length; k++)
-                {
-                    var b = Convert.ToInt32(inputLines[k]);
-                    if (a + b == 2020)
-                    {
-                        return a * b;
-                    }
-                }
-            }
+            var list = inputLines.Select(line => new PlaneTicket(line)).ToList();
 
-            return -1;
+            return list.Max(x => x.SeatId);
         }
 
         /// <summary>
-        /// The Elves in accounting are thankful for your help; one of them even offers you a starfish coin they had left over from a past vacation. They offer you a second one if you can find three numbers in your expense report that meet the same criteria.
-
-        /// Using the above example again, the three entries that sum to 2020 are 979, 366, and 675. Multiplying them together produces the answer, 241861950.
-
-        ///In your expense report, what is the product of the three entries that sum to 2020?
+        /// --- Part Two ---
+        /// Ding! The "fasten seat belt" signs have turned on.Time to find your seat.
+        /// 
+        /// It's a completely full flight, so your seat should be the only missing boarding pass in your list. However, there's a catch: some of the seats at the very front and back of the plane don't exist on this aircraft, so they'll be missing from your list as well.
+        /// 
+        /// Your seat wasn't at the very front or back, though; the seats with IDs +1 and -1 from yours will be in your list.
+        /// 
+        /// What is the ID of your seat?
         /// </summary>
         private int RunPuzzle2()
         {
             var inputLines = GetInputLines();
 
-            for (var i = 0; i < inputLines.Length; i++)
+            var existingTickets = inputLines.Select(line => new PlaneTicket(line)).ToList();
+
+            var missingTickets = new List<Tuple<int, int>>();
+
+            for (var r = 0; r <= 127; r++)
             {
-                var a = Convert.ToInt32(inputLines[i]);
-                for (var k = i; k < inputLines.Length; k++)
+                for (var c = 0; c <= 7; c++)
                 {
-                    var b = Convert.ToInt32(inputLines[k]);
-                    for (var j = k; j < inputLines.Length; j++)
+                    if (!existingTickets.Any(x => x.Row == r && x.Col == c))
                     {
-                        var c = Convert.ToInt32(inputLines[j]);
-                        if (a + b + c == 2020)
-                        {
-                            return a * b * c;
-                        }
+                        missingTickets.Add(new Tuple<int, int>(r, c));
                     }
+                }
+            }
+
+            foreach (var missingTicket in missingTickets)
+            {
+                var missingTicketId = GenerateSeatId(missingTicket.Item1, missingTicket.Item2);
+
+                if (existingTickets.Exists(x => x.SeatId == missingTicketId - 1)
+                    && existingTickets.Exists(x => x.SeatId == missingTicketId + 1))
+                {
+                    return missingTicketId;
                 }
             }
 
             return -1;
         }
+        
+        private int GenerateSeatId(int row, int col)
+        {
+            return row * 8 + col;
+        }
+    }
+
+    public class PlaneTicket
+    {
+        public PlaneTicket(string code)
+        {
+            Code = code;
+        }
+
+        public int Row
+        {
+            get
+            {
+                var rowSection = Code.Substring(0, 7);
+
+                var minRow = 0;
+                var maxRow = 127;
+
+                foreach (var c in rowSection.ToCharArray())
+                {
+                    if (c == 'B')
+                    {
+                        minRow = maxRow - ((maxRow - minRow) / 2);
+                    }
+                    else
+                    {
+                        maxRow = minRow + ((maxRow - minRow) / 2);
+                    }
+                }
+
+                return minRow;
+            }
+        }
+
+        public int Col
+        {
+            get
+            {
+                var colSection = Code.Substring(7);
+
+                var minCol = 0;
+                var maxCol = 7;
+
+                foreach (var c in colSection.ToCharArray())
+                {
+                    if (c == 'R')
+                    {
+                        minCol = maxCol - ((maxCol - minCol) / 2);
+                    }
+                    else
+                    {
+                        maxCol = minCol + ((maxCol - minCol) / 2);
+                    }
+                }
+
+                return minCol;
+            }
+        }
+
+        public int SeatId => Row* 8 + Col;
+        public string Code { get; }
     }
 }
