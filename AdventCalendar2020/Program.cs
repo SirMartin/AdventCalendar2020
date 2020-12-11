95de8805d170400685eeee460c46505964584972
SirMartin <sirmartin@gmail.com>
2020-12-11 09:35:16 +0100

    Finished day 4

--- AdventCalendar2020/Program.cs
+++ AdventCalendar2020/Program.cs
@@ -10,7 +10,7 @@ namespace AdventCalendar2020
             new Day01().Run();
             new Day02().Run();
             new Day03().Run();
-            //new Day04().Run();
+            new Day04().Run();
             //new Day05().Run();
             //new Day06().Run();
             //new Day07().Run();
