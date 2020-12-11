5d1fdee814a1ad244f602a22ab9d73c40426bea7
SirMartin <sirmartin@gmail.com>
2020-12-11 11:35:24 +0100

    Finished day 6

--- AdventCalendar2020/Puzzles/Day06.cs
+++ AdventCalendar2020/Puzzles/Day06.cs
@@ -28,94 +28,158 @@ namespace AdventCalendar2020.Puzzles
         }
 
         /// <summary>
-        /// --- Day 2: Password Philosophy ---
-        ///     Your flight departs in a few days from the coastal airport; the easiest way down to the coast from here is via toboggan.
+        /// --- Day 6: Custom Customs ---
+        /// As your flight approaches the regional airport where you'll switch to a much larger plane, customs declaration forms are distributed to the passengers.
+        
+        /// The form asks a series of 26 yes-or-no questions marked a through z.All you need to do is identify the questions for which anyone in your group answers "yes". Since your group is just you, this doesn't take very long.
+        
+        /// However, the person sitting next to you seems to be experiencing a language barrier and asks if you can help.For each of the people in their group, you write down the questions for which they answer "yes", one per line.For example:
+        
+        ///  abcx
+        ///  abcy
+        ///  abcz
+        /// In this group, there are 6 questions to which anyone answered "yes": a, b, c, x, y, and z. (Duplicate answers to the same question don't count extra; each question counts at most once.)
+        
+        /// Another group asks for your help, then another, and eventually you've collected answers from every group on the plane (your puzzle input). Each group's answers are separated by a blank line, and within each group, each person's answers are on a single line. For example:
+        
+        ///  abc
+        
+        ///  a
+        ///  b
+        ///  c
+        
+        ///  ab
+        ///  ac
+        
+        ///  a
+        ///  a
+        ///  a
+        ///  a
+        
+        ///  b
         /// 
-        ///     The shopkeeper at the North Pole Toboggan Rental Shop is having a bad day. "Something's wrong with our computers; we can't log in!" You ask if you can take a look.
-        /// 
-        ///     Their password database seems to be a little corrupted: some of the passwords wouldn't have been allowed by the Official Toboggan Corporate Policy that was in effect when they were chosen.
-        /// 
-        ///     To try to debug the problem, they have created a list (your puzzle input) of passwords(according to the corrupted database) and the corporate policy when that password was set.
-        /// 
-        ///     For example, suppose you have the following list:
-        /// 
-        /// 1-3 a: abcde
-        /// 1-3 b: cdefg
-        /// 2-9 c: ccccccccc
-        ///     Each line gives the password policy and then the password.The password policy indicates the lowest and highest number of times a given letter must appear for the password to be valid.For example, 1-3 a means that the password must contain a at least 1 time and at most 3 times.
-        /// 
-        ///     In the above example, 2 passwords are valid.The middle password, cdefg, is not; it contains no instances of b, but needs at least 1. The first and third passwords are valid: they contain one a or nine c, both within the limits of their respective policies.
-        /// 
-        ///     How many passwords are valid according to their policies?
+        /// This list represents answers from five groups:
+        
+        /// The first group contains one person who answered "yes" to 3 questions: a, b, and c.
+        /// The second group contains three people; combined, they answered "yes" to 3 questions: a, b, and c.
+        /// The third group contains two people; combined, they answered "yes" to 3 questions: a, b, and c.
+        /// The fourth group contains four people; combined, they answered "yes" to only 1 question, a.
+        /// The last group contains one person who answered "yes" to only 1 question, b.
+        /// In this example, the sum of these counts is 3 + 3 + 3 + 1 + 1 = 11.
+        
+        /// For each group, count the number of questions to which anyone answered "yes". What is the sum of those counts?
         /// </summary>
         private int RunPuzzle1()
         {
-            var validPasswordCount = 0;
+            var numberOfYes = 0;
 
             var inputLines = GetInputLines();
 
+            var groupAnswers = new List<char>();
             foreach (var line in inputLines)
             {
-                var lineParts = line.Split(' ');
-
-                var minMax = lineParts[0].Split('-');
-                var min = Convert.ToInt32(minMax[0]);
-                var max = Convert.ToInt32(minMax[1]);
-
-                var letter = lineParts[1].ToCharArray()[0];
-
-                var count = lineParts[2].Count(x => x == letter);
-
-                if (count >= min && count <= max)
+                if (string.IsNullOrEmpty(line))
                 {
-                    validPasswordCount++;
+                    // New group, clean variables.
+                    numberOfYes += groupAnswers.Count;
+                    groupAnswers = new List<char>();
+                    continue;
+                }
+
+                foreach (var answer in line.ToCharArray())
+                {
+                    if (!groupAnswers.Contains(answer))
+                    {
+                        // Add to the list.
+                        groupAnswers.Add(answer);
+                    }
                 }
             }
 
-            return validPasswordCount;
+            // Count the last group.
+            numberOfYes += groupAnswers.Count;
+
+            return numberOfYes;
         }
 
         /// <summary>
-        ///    --- Part Two ---
-        ///    While it appears you validated the passwords correctly, they don't seem to be what the Official Toboggan Corporate Authentication System is expecting.
+        /// --- Part Two ---
+        /// As you finish the last group's customs declaration, you notice that you misread one word in the instructions:
         ///
-        ///    The shopkeeper suddenly realizes that he just accidentally explained the password policy rules from his old job at the sled rental place down the street! The Official Toboggan Corporate Policy actually works a little differently.
+        /// You don't need to identify the questions to which anyone answered "yes"; you need to identify the questions to which everyone answered "yes"!
         ///
-        ///    Each policy actually describes two positions in the password, where 1 means the first character, 2 means the second character, and so on. (Be careful; Toboggan Corporate Policies have no concept of "index zero"!) Exactly one of these positions must contain the given letter.Other occurrences of the letter are irrelevant for the purposes of policy enforcement.
+        /// Using the same example as above:
         ///
-        ///    Given the same example list from above:
+        /// abc
         ///
-        ///       1-3 a: abcde is valid: position 1 contains a and position 3 does not.
-        ///       1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
-        ///       2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
-        ///    How many passwords are valid according to the new interpretation of the policies?
+        /// a
+        /// b
+        /// c
+        ///
+        /// ab
+        /// ac
+        ///
+        /// a
+        /// a
+        /// a
+        /// a
+        ///
+        /// b
+        /// This list represents answers from five groups:
+        ///
+        /// In the first group, everyone (all 1 person) answered "yes" to 3 questions: a, b, and c.
+        /// In the second group, there is no question to which everyone answered "yes".
+        /// In the third group, everyone answered yes to only 1 question, a.Since some people did not answer "yes" to b or c, they don't count.
+        /// In the fourth group, everyone answered yes to only 1 question, a.
+        /// In the fifth group, everyone (all 1 person) answered "yes" to 1 question, b.
+        /// In this example, the sum of these counts is 3 + 0 + 1 + 1 + 1 = 6.
+        ///
+        /// For each group, count the number of questions to which everyone answered "yes". What is the sum of those counts?
         /// </summary>
         private int RunPuzzle2()
         {
-            var validPasswordCount = 0;
+            var numberOfYes = 0;
 
             var inputLines = GetInputLines();
 
+            var groupAnswers = new List<char>();
+            var firstPersonInGroup = true;
             foreach (var line in inputLines)
             {
-                var lineParts = line.Split(' ');
-
-                var positions = lineParts[0].Split('-');
-                var pos1 = Convert.ToInt32(positions[0]);
-                var pos2 = Convert.ToInt32(positions[1]);
-
-                var letter = lineParts[1].ToCharArray()[0];
-
-                var passwordLetters = lineParts[2].ToCharArray();
-
-                if ((passwordLetters[pos1 - 1] == letter && passwordLetters[pos2 - 1] != letter) ||
-                    (passwordLetters[pos1 - 1] != letter && passwordLetters[pos2 - 1] == letter))
+                if (string.IsNullOrEmpty(line))
                 {
-                    validPasswordCount++;
+                    // New group, clean variables.
+                    numberOfYes += groupAnswers.Count;
+                    groupAnswers.Clear();
+                    firstPersonInGroup = true;
+                    continue;
                 }
+
+                // First person of group.
+                if (firstPersonInGroup)
+                {
+                    firstPersonInGroup = false;
+                    groupAnswers.AddRange(line.ToCharArray());
+                    continue;
+                }
+
+                // Rest of persons of the group.
+                var filteredGroupAnswers = new List<char>();
+                foreach (var answer in groupAnswers)
+                {
+                    if (line.ToCharArray().Contains(answer))
+                    {
+                        filteredGroupAnswers.Add(answer);
+                    }
+                }
+
+                groupAnswers = new List<char>(filteredGroupAnswers);
             }
 
-            return validPasswordCount;
+            // Count the last group.
+            numberOfYes += groupAnswers.Count;
+
+            return numberOfYes;
         }
     }
 }
