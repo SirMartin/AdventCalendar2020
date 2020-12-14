4423e4790e1d9791e126d07db61dc3d69ecb58b8
SirMartin <sirmartin@gmail.com>
2020-12-14 15:00:24 +0100

    Finished day 7, added unit test to the second part, it was really really hard.

--- AdventCalendar2020/Puzzles/Day07.cs
+++ AdventCalendar2020/Puzzles/Day07.cs
@@ -7,6 +7,18 @@ namespace AdventCalendar2020.Puzzles
 {
     public class Day07
     {
+        private string[] _inputLines;
+
+        public Day07()
+        {
+            _inputLines = null;
+        }
+
+        public Day07(string[] inputLines)
+        {
+            _inputLines = inputLines;
+        }
+
         private const string DayNumber = "07";
 
         public void Run()
@@ -16,14 +28,19 @@ namespace AdventCalendar2020.Puzzles
             var result1 = RunPuzzle1();
             stopwatch.Stop();
             Console.WriteLine($"Day {DayNumber} - Puzzle 1: {result1} - Elapsed: {stopwatch.ElapsedMilliseconds} ms");
-            //stopwatch.Restart();
-            //var result2 = RunPuzzle2();
-            //stopwatch.Stop();
-            //Console.WriteLine($"Day {DayNumber} - Puzzle 2: {result2} - Elapsed: {stopwatch.ElapsedMilliseconds} ms");
+            stopwatch.Restart();
+            var result2 = RunPuzzle2();
+            stopwatch.Stop();
+            Console.WriteLine($"Day {DayNumber} - Puzzle 2: {result2} - Elapsed: {stopwatch.ElapsedMilliseconds} ms");
         }
 
         private string[] GetInputLines()
         {
+            if (_inputLines != null)
+            {
+                return _inputLines;
+            }
+
             return System.IO.File.ReadAllLines($@"inputs\day{DayNumber}.txt");
         }
 
@@ -66,7 +83,7 @@ namespace AdventCalendar2020.Puzzles
 
             var colorsToIterate = GetColorsCanCarryMyBag(new List<string>() { myBag }, new List<string>());
 
-            var totalColorsCanCarryMyBag = new List<string>(){};
+            var totalColorsCanCarryMyBag = new List<string>() { };
             totalColorsCanCarryMyBag.AddRange(colorsToIterate);
 
             var foundNewColors = true;
@@ -114,46 +131,184 @@ namespace AdventCalendar2020.Puzzles
         }
 
         /// <summary>
-        ///    --- Part Two ---
-        ///    While it appears you validated the passwords correctly, they don't seem to be what the Official Toboggan Corporate Authentication System is expecting.
+        /// --- Part Two ---
+        /// It's getting pretty expensive to fly these days - not because of ticket prices, but because of the ridiculous number of bags you need to buy!
+        /// 
+        /// Consider again your shiny gold bag and the rules from the above example:
+        /// 
+        /// faded blue bags contain 0 other bags.
+        /// dotted black bags contain 0 other bags.
+        /// vibrant plum bags contain 11 other bags: 5 faded blue bags and 6 dotted black bags.
+        /// dark olive bags contain 7 other bags: 3 faded blue bags and 4 dotted black bags.
         ///
-        ///    The shopkeeper suddenly realizes that he just accidentally explained the password policy rules from his old job at the sled rental place down the street! The Official Toboggan Corporate Policy actually works a little differently.
+        /// So, a single shiny gold bag must contain 1 dark olive bag (and the 7 bags within it) plus 2 vibrant plum bags(and the 11 bags within each of those) : 1 + 1*7 + 2 + 2*11 = 32 bags!
+        /// 
+        /// Of course, the actual rules have a small chance of going several levels deeper than this example; be sure to count all of the bags, even if the nesting becomes topologically impractical!
+        /// 
+        /// Here's another example:
+        /// 
+        /// shiny gold bags contain 2 dark red bags.
+        /// dark red bags contain 2 dark orange bags.
+        /// dark orange bags contain 2 dark yellow bags.
+        /// dark yellow bags contain 2 dark green bags.
+        /// dark green bags contain 2 dark blue bags.
+        /// dark blue bags contain 2 dark violet bags.
+        /// dark violet bags contain no other bags.
         ///
-        ///    Each policy actually describes two positions in the password, where 1 means the first character, 2 means the second character, and so on. (Be careful; Toboggan Corporate Policies have no concept of "index zero"!) Exactly one of these positions must contain the given letter.Other occurrences of the letter are irrelevant for the purposes of policy enforcement.
-        ///
-        ///    Given the same example list from above:
-        ///
-        ///       1-3 a: abcde is valid: position 1 contains a and position 3 does not.
-        ///       1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
-        ///       2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
-        ///    How many passwords are valid according to the new interpretation of the policies?
+        /// In this example, a single shiny gold bag must contain 126 other bags.
+        /// How many individual bags are required inside your single shiny gold bag?
         /// </summary>
         private int RunPuzzle2()
         {
-            var validPasswordCount = 0;
+            var myBag = GetMyBag("shiny gold");
+            return myBag.ContentNumber;
+        }
 
+        #region Bad Approaches
+        internal int GetNumberOfBagsInsideTheBag(string colorToFind)
+        {
             var inputLines = GetInputLines();
+
+            var colorLine = inputLines.First(x => x.StartsWith(colorToFind));
+            var content = colorLine.Substring(colorLine.IndexOf("contain") + "contain".Length).Trim().Replace(".", "");
+            var bags = content.Split(',').Select(x => x.Trim());
+
+            var num = 0;
+            foreach (var bag in bags)
+            {
+                if (bag == "no other bags")
+                {
+                    return 0;
+                }
+
+                var keyValueSeparator = bag.IndexOf(' ');
+                var value = Convert.ToInt32(bag.Substring(0, keyValueSeparator).Trim());
+                var key = bag.Substring(keyValueSeparator).Replace("bags", string.Empty).Replace("bag", string.Empty).Trim();
+
+                var result = GetNumberOfBagsInsideTheBag(key);
+
+                if (result == 0)
+                {
+                    num += value;
+                    continue;
+                }
+
+                return value * result;
+            }
+
+            return num;
+        }
+
+        private int GetNumberOfBagsFromThisBag(string colorCarried, int previousAmount)
+        {
+            var inputLines = GetInputLines();
+
+            var totalBags = 0;
 
             foreach (var line in inputLines)
             {
-                var lineParts = line.Split(' ');
-
-                var positions = lineParts[0].Split('-');
-                var pos1 = Convert.ToInt32(positions[0]);
-                var pos2 = Convert.ToInt32(positions[1]);
-
-                var letter = lineParts[1].ToCharArray()[0];
-
-                var passwordLetters = lineParts[2].ToCharArray();
-
-                if ((passwordLetters[pos1 - 1] == letter && passwordLetters[pos2 - 1] != letter) ||
-                    (passwordLetters[pos1 - 1] != letter && passwordLetters[pos2 - 1] == letter))
+                if (line.StartsWith(colorCarried))
                 {
-                    validPasswordCount++;
+                    var content = line.Substring(line.IndexOf("contain") + "contain".Length).Trim().Replace(".", "");
+                    var bags = content.Split(',');
+
+                    var numberOfVags = 0;
+                    foreach (var bag in bags)
+                    {
+                        var theBag = bag.Trim();
+                        if (theBag == "no other bags")
+                        {
+                            return 1;
+                        }
+
+                        var keyValueSeparator = theBag.IndexOf(' ');
+                        var value = Convert.ToInt32(theBag.Substring(0, keyValueSeparator).Trim());
+                        var key = theBag.Substring(keyValueSeparator).Replace("bags", string.Empty).Replace("bag", string.Empty).Trim();
+
+                        // Get the inside ones.
+                        var result = GetNumberOfBagsFromThisBag(key, value);
+                        numberOfVags += value * result;
+                    }
+
+                    totalBags += previousAmount + (previousAmount == 0 ? 1 : previousAmount * numberOfVags);
                 }
             }
 
-            return validPasswordCount;
+            return totalBags;
+        }
+        #endregion
+
+        public Bag GetMyBag(string color)
+        {
+            var myBag = new Bag(color, true);
+            myBag.Content = GetBagContent(myBag.Color);
+            return myBag;
+        }
+
+        private List<Bag> GetBagContent(string color)
+        {
+            var bagList = new List<Bag>();
+
+            var inputLines = GetInputLines();
+            var colorLine = inputLines.First(x => x.StartsWith(color));
+            var content = colorLine.Substring(colorLine.IndexOf("contain") + "contain".Length).Trim().Replace(".", "");
+            var bags = content.Split(',').Select(x => x.Trim());
+            foreach (var bag in bags)
+            {
+                if (bag == "no other bags")
+                {
+                    return null;
+                }
+
+                var keyValueSeparator = bag.IndexOf(' ');
+                var value = Convert.ToInt32(bag.Substring(0, keyValueSeparator).Trim());
+                var key = bag.Substring(keyValueSeparator).Replace("bags", string.Empty).Replace("bag", string.Empty).Trim();
+
+                var theBag = new Bag(key, value);
+                theBag.Content = GetBagContent(key);
+                bagList.Add(theBag);
+            }
+
+            return bagList;
+        }
+    }
+
+    public class Bag
+    {
+        public Bag(string color, bool isMainBag = false)
+        {
+            Color = color;
+            IsMainBag = isMainBag;
+        }
+
+        public Bag(string color, int number) : this(color, false)
+        {
+            Number = number;
+        }
+
+        public int Number { get; set; }
+        public string Color { get; set; }
+        public List<Bag> Content { get; set; }
+
+        public bool IsMainBag { get; set; }
+
+        public int ContentNumber
+        {
+            get
+            {
+                var contentTotal = 0;
+                if (Content != null)
+                {
+                    contentTotal = Content.Sum(x => x.ContentNumber);
+                }
+
+                if (IsMainBag)
+                {
+                    return contentTotal;
+                }
+
+                return Number + Number * contentTotal;
+            }
         }
     }
 }
---
+++ AdventCalendar2020/Tests/Day7Tests.cs
@@ -0,0 +1,56 @@
+﻿using AdventCalendar2020.Puzzles;
+using NUnit.Framework;
+
+namespace AdventCalendar2020.Tests
+{
+    [TestFixture]
+    public class Day7Tests
+    {
+        private Day07 _day07;
+
+        [Test]
+        public void ShinyGold_NoBagsInside_Returns0()
+        {
+            var inputLines = System.IO.File.ReadAllLines($@"Tests\Inputs\day07_ShinyGold_NoBagsInside_Returns0.txt");
+            _day07 = new Day07(inputLines);
+            var myBag = _day07.GetMyBag("shiny gold");
+            Assert.AreEqual(0, myBag.ContentNumber);
+        }
+
+        [Test]
+        public void ShinyGold_2DifferentEmptyBagsInside_Returns2()
+        {
+            var inputLines = System.IO.File.ReadAllLines($@"Tests\Inputs\day07_ShinyGold_2DifferentEmptyBagsInside_Returns2.txt");
+            _day07 = new Day07(inputLines);
+            var myBag = _day07.GetMyBag("shiny gold");
+            Assert.AreEqual(2, myBag.ContentNumber);
+        }
+
+        [Test]
+        public void ShinyGold_2BagLevels_Returns3()
+        {
+            var inputLines = System.IO.File.ReadAllLines($@"Tests\Inputs\ShinyGold_2BagLevels_Returns3.txt");
+            _day07 = new Day07(inputLines);
+            var myBag = _day07.GetMyBag("shiny gold");
+            Assert.AreEqual(3, myBag.ContentNumber);
+        }
+
+        [Test]
+        public void ShinyGold_Example1_Returns32()
+        {
+            var inputLines = System.IO.File.ReadAllLines($@"Tests\Inputs\day07_BasicTest1.txt");
+            _day07 = new Day07(inputLines);
+            var myBag = _day07.GetMyBag("shiny gold");
+            Assert.AreEqual(32, myBag.ContentNumber);
+        }
+
+        [Test]
+        public void ShinyGold_Example1_Returns126()
+        {
+            var inputLines = System.IO.File.ReadAllLines($@"Tests\Inputs\day07_BasicTest2.txt");
+            _day07 = new Day07(inputLines);
+            var myBag = _day07.GetMyBag("shiny gold");
+            Assert.AreEqual(126, myBag.ContentNumber);
+        }
+    }
+}
